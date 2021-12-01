3119cd23c1d6c8e2926ebb2fee335435d6a71277
Tamas Kurics <tamas.kurics@gmail.com>
2021-12-01 08:55:50 +0100

    day 1
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ 2021/Haskell/day_01a.hs
@@ -0,0 +1,17 @@
+
+nrIncreasedDepths :: [Int] -> Int
+nrIncreasedDepths depths =
+    let go :: Int -> [Int] -> Int
+        go acc [] = acc
+        go acc [_] = acc
+        go acc (a : b : dss) =
+            let acc' = if b > a then acc + 1 else acc
+            in go acc' (b : dss)
+    in go 0 depths
+
+
+main :: IO()
+main = do
+    content <- fmap lines (readFile "../resources/input_01.txt")
+    let depths = map read content
+    print $ nrIncreasedDepths depths
---
+++ 2021/Haskell/day_01b.hs
@@ -0,0 +1,23 @@
+
+slidingWindow :: [Int] -> [(Int, Int, Int)]
+slidingWindow (a : b : c : xs) = (a, b, c) : slidingWindow (b : c : xs)
+slidingWindow _ = []
+
+
+nrIncreasedDepths :: [Int] -> Int
+nrIncreasedDepths depths =
+    let sums = map (\(a, b, c) -> a + b + c) $ slidingWindow depths
+        go :: Int -> [Int] -> Int
+        go acc [] = acc
+        go acc [_] = acc
+        go acc (a : b : xs) =
+            let acc' = if b > a then acc + 1 else acc
+            in go acc' (b : xs)
+    in go 0 sums
+
+
+main :: IO()
+main = do
+    content <- fmap lines (readFile "../resources/input_01.txt")
+    let depths = map read content
+    print $ nrIncreasedDepths depths
